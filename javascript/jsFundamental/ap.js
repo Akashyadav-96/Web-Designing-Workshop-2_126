bbbee4d7cd69e1bf41f3b8d3e42877a04a8456dd
Akashyadav-96 <yadavakash0031@gmail.com>
2026-04-16 00:04:01 +0530

    files commited

---
+++ javascript/jsFundamental/ap.js
@@ -0,0 +1,57 @@
+//var let const--
+
+// var name = "Akash";
+// var age = 18;
+// var isEmployed = true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// var age = 50;
+// console.log(age);
+
+// let name = "Akash";
+// let age = 18;
+// let isEmployed = true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// let age = 50;
+// console.log(age);
+
+// const name = "Akash";
+// const age = 18;
+// const isEmployed = true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// const age = 50;
+// console.log(age);
+
+// if(true){
+//     var age =30; 
+// }
+//     console.log(age);
+
+// if (true) {
+//     let age = 30;
+// }
+// console.log(age);
+
+// const PI=3.14;
+// console.log(PI);
+// PI=3.14159;  //this is will cause an error
+// console.log(PI);
+
+// //types of operator
+
+// console.log(name);
+// console.log(age);
+// console.log(isEmpolyed);
+
+//variable inside string
+
+// let name1 = "Akash";
+// let age1 = 18;
+// console.log("my name is " + name1 + " and my age is " + age1);
+// console.log(`my name is ${name1} and my age is ${age1}`);
+
---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+// var let const
+
+let  a = [1,3,4];
+let  b = a;
+b.pop();
