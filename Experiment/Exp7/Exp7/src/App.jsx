88f4bcd573a7529832df4d0d9cb178f32bfbd996
Akashyadav-96 <yadavakash0031@gmail.com>
2026-06-19 04:05:00 +0530

    file commit

--- Experiment/Exp7/Exp7/src/App.jsx
+++ Experiment/Exp7/Exp7/src/App.jsx
@@ -11,9 +11,9 @@ function App() {
       <div className="card-container"> 
         <Students name="Akash Yadav" age={18} physics={87} chemistry={93} maths={71} /> 
         <Students name="Shalley Sharma" age={19} physics={92} chemistry={86} maths={99} /> 
-        <Students name="Sanjay Verma" age={18} physics={88} chemistry={82} maths={91} /> 
+        <Students name="Aditya Mishra" age={18} physics={88} chemistry={82} maths={91} /> 
         <Students name="Ankita Singh" age={21} physics={71} chemistry={65} maths={70} /> 
-        <Students name="Rahul Gupta" age={19} physics={92} chemistry={91} maths={95} /> 
+        <Students name="Akshita Yadav" age={19} physics={92} chemistry={91} maths={95} /> 
       </div> 
     </div> 
   ); 
